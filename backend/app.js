f966c5d466889b902cd7a9bc787790fe615ee82f
springerjanek <springerjan2@gmail.com>
2025-11-14 16:01:39 +0100

    login functionality

--- backend/app.js
+++ backend/app.js
@@ -2,7 +2,7 @@ import express from "express";
 import { generateAccessToken } from "./jwt.js";
 import bcrypt from "bcrypt";
 import { body, validationResult } from "express-validator";
-import { createUser } from "./queries/users.js";
+import { createUser, getUserByUsername } from "./queries/users.js";
 
 // GET https://example.com:4000/api/userOrders
 // Authorization: Bearer JWT_ACCESS_TOKEN
@@ -29,11 +29,9 @@ app.post(
     if (!errors.isEmpty())
       return res.status(400).json({ errors: errors.array() });
 
+    const { username, password } = req.body;
+
     try {
-      const { username, password } = req.body;
-
-      const token = generateAccessToken({ username: username });
-
       const hashedPassword = await bcrypt.hash(password, 10);
 
       const result = await createUser(username, hashedPassword);
@@ -42,7 +40,6 @@ app.post(
 
       res.status(201).json({
         user: newUser,
-        token: token,
       });
     } catch (err) {
       console.error(err);
@@ -53,6 +50,49 @@ app.post(
       }
 
       return res.status(500).json({ error: err });
+    }
+  }
+);
+
+app.post(
+  "/api/login",
+  [
+    body("username")
+      .isLength({ min: 6 })
+      .withMessage("Username must be at least 6 characters long"),
+    body("password")
+      .isLength({ min: 6 })
+      .withMessage("Password must be at least 6 characters long"),
+  ],
+  async (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty())
+      return res.status(400).json({ errors: errors.array() });
+
+    const { username, password } = req.body;
+
+    try {
+      const result = await getUserByUsername(username);
+      const user = result.rows[0];
+
+      if (!user) {
+        return res.status(401).json({ error: "Invalid username" });
+      }
+
+      const match = await bcrypt.compare(password, user.password);
+      if (!match) {
+        return res.status(401).json({ error: "Invalid password" });
+      }
+
+      const token = generateAccessToken({ username: user.username });
+
+      res.json({
+        user: { id: user.id, username: user.username },
+        token: token,
+      });
+    } catch (err) {
+      console.error(err);
+      res.status(500).json({ error: "Server error" });
     }
   }
 );
--- backend/queries/users.js
+++ backend/queries/users.js
@@ -7,3 +7,7 @@ export const createUser = async (username, password) =>
        RETURNING id, username, role, created_at;`,
     [username, password, "user"]
   );
+
+export const getUserByUsername = (username) => {
+  return db.query("SELECT * FROM users WHERE username = $1", [username]);
+};
