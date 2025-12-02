b4084e9b5ae9b6052e86b0a380dacc2977b5b0b9
springerjanek <springerjan2@gmail.com>
2025-12-02 17:38:10 +0100

    showing active users

--- frontend/psw/src/Navbar.jsx
+++ frontend/psw/src/Navbar.jsx
@@ -7,26 +7,36 @@ import {
 import { Link } from "react-router";
 import { useAuth } from "./auth/AuthContext";
 import { Button } from "./components/ui/button";
+import { PulseIcon } from "./components/ui/icons/svg-spinners-pulse";
+import { useSocket } from "./context/SocketContext";
 
 export const Navbar = () => {
   const { logout } = useAuth();
+  const { userCount } = useSocket();
   return (
     <NavigationMenu>
       <NavigationMenuList>
+        <div className="flex">
+          <NavigationMenuItem>
+            <NavigationMenuLink asChild>
+              <Link to="/">Home</Link>
+            </NavigationMenuLink>
+          </NavigationMenuItem>
+          <NavigationMenuItem>
+            <NavigationMenuLink asChild>
+              <Link to="/rooms">Rooms</Link>
+            </NavigationMenuLink>
+          </NavigationMenuItem>
+          <NavigationMenuItem>
+            <Button variant="ghost" onClick={logout}>
+              Logout
+            </Button>
+          </NavigationMenuItem>
+        </div>
         <NavigationMenuItem>
-          <NavigationMenuLink asChild>
-            <Link to="/">Home</Link>
-          </NavigationMenuLink>
-        </NavigationMenuItem>
-        <NavigationMenuItem>
-          <NavigationMenuLink asChild>
-            <Link to="/rooms">Rooms</Link>
-          </NavigationMenuLink>
-        </NavigationMenuItem>
-        <NavigationMenuItem>
-          <Button variant="ghost" onClick={logout}>
-            Logout
-          </Button>
+          <div className="flex gap-1.5">
+            Active Users: {userCount} <PulseIcon color="green" />
+          </div>
         </NavigationMenuItem>
       </NavigationMenuList>
     </NavigationMenu>
