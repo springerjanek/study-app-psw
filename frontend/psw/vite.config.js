239de66ee555699422dfee440fe14be14576f319
springerjanek <springerjan2@gmail.com>
2026-01-02 15:37:12 +0100

    tls certificate

--- frontend/psw/vite.config.js
+++ frontend/psw/vite.config.js
@@ -2,6 +2,7 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
 import path from "path";
+import fs from "fs";
 
 // https://vite.dev/config/
 export default defineConfig({
@@ -10,5 +11,12 @@ export default defineConfig({
     alias: {
       "@": path.resolve(__dirname, "./src"),
     },
+  },
+  server: {
+    https: {
+      key: fs.readFileSync("./localhost-key.pem"),
+      cert: fs.readFileSync("./localhost.pem"),
+    },
+    port: 5173,
   },
 });
