3b6ab75a7509d7d97a27075353e7bdacfd815cfd
springerjanek <springerjan2@gmail.com>
2026-01-02 14:48:08 +0100

    more info on main page

--- frontend/psw/src/App.jsx
+++ frontend/psw/src/App.jsx
@@ -1,12 +1,20 @@
+import { Link } from "react-router";
 import { useAuth } from "./auth/AuthContext";
 
 function App() {
   const { user } = useAuth();
 
   return (
-    <>
+    <div className="flex flex-col items-center">
       <h1>Welcome, {user.username}!</h1>
-    </>
+      <p>
+        Please visit{" "}
+        <Link className="underline mr-1" to="/rooms">
+          Rooms
+        </Link>
+        to get started.
+      </p>
+    </div>
   );
 }
 
