ad4c9e4f7345ee510a7b25e0c2dc704c3cd3e292
springerjanek <springerjan2@gmail.com>
2026-01-07 20:31:45 +0100

    /deleteUser

--- backend/routes/user.routes.js
+++ backend/routes/user.routes.js
@@ -1,5 +1,5 @@
 import express from "express";
-import { getAllUsers } from "../queries/users.js";
+import { deleteUserById, getAllUsers } from "../queries/users.js";
 import { validateToken } from "../jwt.js";
 
 const router = express.Router();
@@ -8,6 +8,23 @@ router.get("/users", validateToken, async (req, res) => {
   try {
     const result = await getAllUsers();
     res.json(result.rows);
+  } catch (err) {
+    console.error(err);
+    res.status(500).json({ error: "Server error" });
+  }
+});
+
+router.delete("/deleteUser", validateToken, async (req, res) => {
+  try {
+    const userRole = req.user.role;
+    const { user_id } = req.body;
+
+    if (userRole !== "admin") {
+      return res.status(403).json({ error: "Only admin can delete users" });
+    }
+
+    await deleteUserById(user_id);
+    res.json({ success: true });
   } catch (err) {
     console.error(err);
     res.status(500).json({ error: "Server error" });
